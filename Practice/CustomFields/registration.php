d5c6c0a29abcc6dfe7663eda8228afc3f305cc15
root <root@dev.marketplace.com>
2021-05-14 21:26:21 +0530

    customfields

---
+++ Practice/CustomFields/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Lsi Counselors
+ *
+ * @category  Practice
+ * @package   Practice_MessageQueue
+ * @author    Practice
+ * @copyright Copyright (c) Lsi Software Private Limited 
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Practice_CustomFields',
+    __DIR__
+);
